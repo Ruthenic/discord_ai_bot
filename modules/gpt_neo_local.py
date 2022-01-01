5f6227e20097f9c5a4e51facbe39ac26e4e7c076
Ruthenic <mdrakea3@tutanota.com>
2022-01-01 16:33:26 -0500

    [modules] Add even more janky module for local neo

---
+++ modules/gpt_neo_local.py
@@ -0,0 +1,59 @@
+import requests, re 
+from transformers.pipelines import pipeline
+
+class completion():
+    def __init__(self, config):
+        self.API_URL = "https://api-inference.huggingface.co/models/" + "EleutherAI/gpt-neo-2.7B"
+        self.headers = {"Authorization": "Bearer " + config["huggingface"]["token"]}
+        self.NAME = config["general"]["completion"]["defaults"]["name"]
+        self.BACKSTORY = config["general"]["completion"]["defaults"]["backstory"]
+        self.generator = pipeline('text-generation', model='EleutherAI/gpt-neo-125M')
+        self.name = self.NAME           # required for compat with the rest
+        self.backstory = self.BACKSTORY
+        self.prefix = 'The following is a chat with ' + self.NAME + ', ' + self.BACKSTORY + '.\n'
+        self.memory = []
+
+    def query(self, payload):
+        prompt = payload["inputs"]
+        res = self.generator(prompt, do_sample=True, max_length=100)
+        return res
+
+    def genCleanMessage(self, optPrompt, userName):
+        completePrompt = self.prefix + '\n'
+        for turn in self.memory[-3:]:
+            completePrompt += turn['User'] + ': "' + turn['Prompt'] + '"\n' + self.NAME + ': "' + turn['Reply'] + '"\n'
+        completePrompt += userName + ': "' + optPrompt + '"\n' + self.NAME + ': "'
+        print('\nPROMPT:\n' + completePrompt)
+        text_generation_parameters = {
+            'max_new_tokens': 50,
+            'temperature': 0.8,
+            'repetition_penalty': 1.8,
+            'top_k': 40,
+            'return_full_text': False
+        }
+        output_list = self.query({"inputs": completePrompt, "parameters": text_generation_parameters})
+        response = output_list[0]["generated_text"].replace(completePrompt, "")
+        print('\nGENERATED:\n' + response)
+        truncate = 0
+        cleanStr = ''
+        truncate = response.find('"')
+        cleanStr = response[:truncate]
+        if re.search(r'[?.!]', cleanStr):
+            trimPart = re.split(r'[?.!]', cleanStr)[-1]
+            cleanStr = cleanStr.replace(trimPart,'')
+        print('\nEXTRACTED:\n' + cleanStr)
+        if not cleanStr:
+            cleanStr = 'Idk how to respond to that lol. I broke.'
+        self.memory.append({'User': userName, 'Prompt': optPrompt, 'Reply': cleanStr})
+        return cleanStr
+    def changeShit(self, name=None, backstory=None):
+        self.NAME = name if name else self.NAME
+        self.BACKSTORY = backstory if backstory else self.BACKSTORY
+        self.name = self.NAME           # required for compat with the rest
+        self.backstory = self.BACKSTORY
+        print(f"New name: {name}\nNew backstory: {backstory}")
+        self.prefix = 'The following is a chat with ' + self.NAME + ', ' + self.BACKSTORY + '.\n'
+        self.memory = []
+    def complete(self, username, text):
+        genMessage = self.genCleanMessage(text, username)
+        return f"[{self.NAME}] {genMessage}"
